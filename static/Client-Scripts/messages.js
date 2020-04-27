f097e955a08cbf247b0dcd6f2647e5e600a40410
N8Grant <nathngrant@umass.edu>
2020-04-27 13:25:44 -0400

    added created messages.js and added to the server

---
+++ static/Client-Scripts/messages.js
@@ -0,0 +1,223 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var myURL = "http://localhost:8080/";
+var conversations = [];
+var selected = 0;
+/*
+messageData = {
+    'result': 'success',
+    'conversations': [
+        {
+            'username':____
+            'date': ___
+            'messages': [
+                {
+                    'type': 'received',
+                    'content': 'Hello can i buy the bio book?'
+                }
+            ]
+        }
+    ]
+}
+*/
+window.onload = function () {
+    var sm = document.getElementById('send-message-button');
+    sm.addEventListener('click', sendMessage);
+    loadConversations();
+};
+var parseCookie = function (str) {
+    return str
+        .split(';')
+        .map(function (v) { return v.split('='); })
+        .reduce(function (acc, v) {
+        acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
+        return acc;
+    }, {});
+};
+function loadConversations() {
+    return __awaiter(this, void 0, void 0, function () {
+        var newURL, cookie, cookieObj, newURL_1, uname, resp, responseJson, i, toInsert, view, toInsert, view;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    newURL = myURL + "messages/";
+                    cookie = document.cookie;
+                    if (cookie == "") {
+                        alert("Please Log In!");
+                        location.replace(myURL);
+                    }
+                    cookieObj = parseCookie(cookie);
+                    if (!(cookieObj.username == null)) return [3 /*break*/, 1];
+                    alert("Please Log In!");
+                    location.replace(myURL);
+                    return [3 /*break*/, 4];
+                case 1:
+                    newURL_1 = myURL + "messages/";
+                    uname = cookieObj.username;
+                    return [4 /*yield*/, fetch(newURL_1, {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify({
+                                'username': uname
+                            })
+                        })];
+                case 2:
+                    resp = _a.sent();
+                    return [4 /*yield*/, resp.json()];
+                case 3:
+                    responseJson = _a.sent();
+                    if (responseJson['result'] != 'success')
+                        alert("Error while sorting");
+                    else {
+                        alert("Sort stuff");
+                        conversations = responseJson['conversations'];
+                        if (conversations.length > 0) {
+                            for (i = 0; i < conversations.length; i++) {
+                                toInsert = "<a onclick='selectConversation(" + i + ")' class='list-group-item list-group-item-action list-group-item-light rounded-0'> \
+                                        <div class='media'> \
+                                            <div class='media-body ml-4'> \
+                                                <div class='d-flex align-items-center justify-content-between mb-1'> \
+                                                    <h6 class='mb-0'>" + conversations[i]['username'] + "</h6><small class='small font-weight-bold'>" + conversations[i]['date'] + "</small> \
+                                                </div> \
+                                            </div> \
+                                        </div> \
+                                    </a>";
+                                view = document.getElementById('messages-box');
+                                view.insertAdjacentHTML('beforeend', toInsert);
+                            }
+                        }
+                        else {
+                            toInsert = "<p>No Messages</p>";
+                            view = document.getElementById('messages-box');
+                            view.insertAdjacentHTML('beforeend', toInsert);
+                        }
+                    }
+                    _a.label = 4;
+                case 4: return [2 /*return*/];
+            }
+        });
+    });
+}
+function selectConversation(num) {
+    selected = num;
+    for (var i = 0; i < conversations.length; i++) {
+        var toInsert = "";
+        if (i == num) {
+            var toInsert_1 = "<a onclick='selectConversation(" + i + ")' class='list-group-item list-group-item-action active text-white rounded-0'> \
+                                <div class='media'> \
+                                    <div class='media-body ml-4'> \
+                                        <div class='d-flex align-items-center justify-content-between mb-1'> \
+                                            <h6 class='mb-0'>" + conversations[i]['username'] + "</h6><small class='small font-weight-bold'>" + conversations[i]['date'] + "</small> \
+                                        </div> \
+                                    </div> \
+                                </div> \
+                            </a> ";
+        }
+        else {
+            var toInsert_2 = "<a onclick='selectConversation(" + i + ")' class='list-group-item list-group-item-action list-group-item-light rounded-0'> \
+                                <div class='media'> \
+                                    <div class='media-body ml-4'> \
+                                        <div class='d-flex align-items-center justify-content-between mb-1'> \
+                                            <h6 class='mb-0'>" + conversations[i]['username'] + "</h6><small class='small font-weight-bold'>" + conversations[i]['date'] + "</small> \
+                                        </div> \
+                                    </div> \
+                                </div> \
+                            </a> ";
+        }
+        var view = document.getElementById('messages-box');
+        view.insertAdjacentHTML('beforeend', toInsert);
+        for (var i_1 = 0; i_1 < conversations['messges'].length; i_1++) {
+            if (conversations['messages'][i_1] == 'received') {
+                displayWhiteMessage(conversations['messages'][i_1]['content']);
+            }
+            else {
+                displayBlueMessage(conversations['messages'][i_1]['content']);
+            }
+        }
+    }
+}
+function displayBlueMessage(message) {
+    var toInsert = "<div class='media w-50 mb-3'> \
+    <div class='media-body'> \
+        d<div class='bg-primary rounded py-2 px-3 mb-2'> \
+            <p class='text-small mb-0 text-white'>" + message + "</p> \
+            </div> \
+        </div> \
+    </div>";
+    var view = document.getElementById('message-box');
+    view.insertAdjacentHTML('beforeend', toInsert);
+}
+function displayWhiteMessage(message) {
+    var toInsert = "<div class='media w-50 mb-3'> \
+        <div class='media-body ml-3'> \
+            d<div class='bg-light rounded py-2 px-3 mb-2'> \
+                <p class='text-small mb-0 text-muted'>" + message + "</p> \
+            </div> \
+        </div> \
+    </div>";
+    var view = document.getElementById('message-box');
+    view.insertAdjacentHTML('beforeend', toInsert);
+}
+function sendMessage() {
+    var _this = this;
+    (function () { return __awaiter(_this, void 0, void 0, function () {
+        var message, newURL, resp;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    message = document.getElementById("send-message-bar").value;
+                    displayBlueMessage(message);
+                    newURL = myURL + "messages/";
+                    return [4 /*yield*/, fetch(newURL, {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify({
+                                'user': conversations['messages'][selected]['content'],
+                                'message': message
+                            })
+                        })];
+                case 1:
+                    resp = _a.sent();
+                    return [2 /*return*/];
+            }
+        });
+    }); });
+}
---
+++ static/Client-Scripts/messages.ts
@@ -0,0 +1,182 @@
+let myURL = "http://localhost:8080/";
+
+let conversations = [];
+let selected = 0;
+/*
+messageData = {
+    'result': 'success',
+    'conversations': [
+        {
+            'username':____
+            'date': ___
+            'messages': [
+                {
+                    'type': 'received',
+                    'content': 'Hello can i buy the bio book?'
+                }
+            ]
+        }
+    ]
+}
+*/
+
+window.onload = function(){
+    let sm = document.getElementById('send-message-button');
+    sm.addEventListener('click', sendMessage);
+    loadConversations();
+}
+
+let parseCookie = str =>
+  str
+    .split(';')
+    .map(v => v.split('='))
+    .reduce((acc, v) => {
+      acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
+      return acc;
+    }, {});
+
+async function loadConversations() {
+    const newURL = myURL + "messages/";
+    var cookie = document.cookie;
+    if(cookie == ""){
+        alert("Please Log In!");
+        location.replace(myURL);
+    }
+    var cookieObj = parseCookie(cookie);
+    if( cookieObj.username == null){
+        alert("Please Log In!");
+        location.replace(myURL);
+    } else {
+        const newURL = myURL + "messages/";
+        let uname = cookieObj.username;
+    
+        const resp = await fetch(newURL, 
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(
+                    {
+                        'username': uname
+                    }
+                )
+            });
+        const responseJson = await resp.json(); 
+
+        if(responseJson['result'] != 'success')
+            alert("Error while sorting");
+        else {
+            alert("Sort stuff");
+            conversations = responseJson['conversations'];
+            if (conversations.length > 0) {
+                for (let i = 0;i < conversations.length; i++) {
+                    let toInsert = "<a onclick='selectConversation("+i+")' class='list-group-item list-group-item-action list-group-item-light rounded-0'> \
+                                        <div class='media'> \
+                                            <div class='media-body ml-4'> \
+                                                <div class='d-flex align-items-center justify-content-between mb-1'> \
+                                                    <h6 class='mb-0'>"+conversations[i]['username']+"</h6><small class='small font-weight-bold'>"+conversations[i]['date']+"</small> \
+                                                </div> \
+                                            </div> \
+                                        </div> \
+                                    </a>";
+                    
+                    let view = document.getElementById('messages-box');
+                    view.insertAdjacentHTML('beforeend', toInsert);
+                }
+            } else {
+                let toInsert = "<p>No Messages</p>";
+                let view = document.getElementById('messages-box');
+                view.insertAdjacentHTML('beforeend', toInsert);
+            }   
+        }
+    }
+}
+
+function selectConversation(num) {
+    selected = num;
+    for (let i = 0;i < conversations.length; i++) {
+        let toInsert = "";
+        if (i == num) {
+            let toInsert = "<a onclick='selectConversation("+i+")' class='list-group-item list-group-item-action active text-white rounded-0'> \
+                                <div class='media'> \
+                                    <div class='media-body ml-4'> \
+                                        <div class='d-flex align-items-center justify-content-between mb-1'> \
+                                            <h6 class='mb-0'>"+conversations[i]['username']+"</h6><small class='small font-weight-bold'>"+conversations[i]['date']+"</small> \
+                                        </div> \
+                                    </div> \
+                                </div> \
+                            </a> ";
+        } else {
+            let toInsert = "<a onclick='selectConversation("+i+")' class='list-group-item list-group-item-action list-group-item-light rounded-0'> \
+                                <div class='media'> \
+                                    <div class='media-body ml-4'> \
+                                        <div class='d-flex align-items-center justify-content-between mb-1'> \
+                                            <h6 class='mb-0'>"+conversations[i]['username']+"</h6><small class='small font-weight-bold'>"+conversations[i]['date']+"</small> \
+                                        </div> \
+                                    </div> \
+                                </div> \
+                            </a> ";
+        }
+        let view = document.getElementById('messages-box');
+        view.insertAdjacentHTML('beforeend', toInsert);
+
+        for (let i = 0; i < conversations['messges'].length; i++) {
+            if (conversations['messages'][i] == 'received') {
+                displayWhiteMessage(conversations['messages'][i]['content']);
+            } else {
+                displayBlueMessage(conversations['messages'][i]['content']);
+            }
+        }
+    }
+}
+
+function displayBlueMessage(message) {
+    let toInsert = "<div class='media w-50 mb-3'> \
+    <div class='media-body'> \
+        d<div class='bg-primary rounded py-2 px-3 mb-2'> \
+            <p class='text-small mb-0 text-white'>" + message + "</p> \
+            </div> \
+        </div> \
+    </div>";
+
+    let view = document.getElementById('message-box');
+    view.insertAdjacentHTML('beforeend', toInsert);
+}
+
+function displayWhiteMessage(message) {
+    let toInsert = "<div class='media w-50 mb-3'> \
+        <div class='media-body ml-3'> \
+            d<div class='bg-light rounded py-2 px-3 mb-2'> \
+                <p class='text-small mb-0 text-muted'>" + message + "</p> \
+            </div> \
+        </div> \
+    </div>";
+
+    let view = document.getElementById('message-box');
+    view.insertAdjacentHTML('beforeend', toInsert);
+}
+
+function sendMessage() {
+    (async () => {
+        let message = (<HTMLInputElement>document.getElementById("send-message-bar")).value;
+        displayBlueMessage(message);
+
+        const newURL = myURL + "messages/";
+        const resp = await fetch(newURL, 
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(
+                    {
+                        'user': conversations['messages'][selected]['content'],
+                        'message': message
+                    }
+                )
+            }
+        );
+        
+    })
+}
